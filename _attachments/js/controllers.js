8c6c3fae325045edc68d79a4396ed2f9934a2309
Steven-Cleys <stevencleys@gmail.com>
2014-11-14 03:46:08 +0100

    fixed stuff
    mapcontroller code temp fixed
    button added
    added button css
    removed nav-bar in index.html

--- _attachments/js/controllers.js
+++ _attachments/js/controllers.js
@@ -1,103 +1,117 @@
 var myApp = angular.module('stapp.controllers', [])
 
-myApp.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-      function initialize() {
-        var myLatlng = new google.maps.LatLng(51.21968667200008,4.4009229560000449); //to do, current coords
-        
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"),
-            mapOptions);
-        
-        //Marker + infowindow + angularjs compiled ng-click
-        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-        var compiled = $compile(contentString)($scope);
+myApp
+		.controller(
+				'MapCtrl',
+				function($scope, $ionicLoading, $compile, $state) {
+					function initialize() {
+						var myLatlng = new google.maps.LatLng(
+								51.21968667200008, 4.4009229560000449); // to
+						// do,
+						// current
+						// coords
 
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
+						var mapOptions = {
+							center : myLatlng,
+							zoom : 16,
+							mapTypeId : google.maps.MapTypeId.ROADMAP
+						};
+						var map = new google.maps.Map(document
+								.getElementById("map"), mapOptions);						
 
+//						google.maps.event.addListener(marker, 'click',
+//								function() {
+//									infowindow.open(map, marker);
+//								});
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
+						$scope.map = map;
+					}
+					
+					
 
-        $scope.map = map;
-      }
-      
-      initialize();
-      
-      function showspinner() {
-    	    $ionicLoading.show({
-    	      content: "whatever"
-    	    });
-    	  };
-    	  $scope.hide = function(){
-    	    $ionicLoading.hide();
-    	  };
-    	  
-    showspinner();
-      
-      $scope.centerOnMe = function() {
-        if(!$scope.map) {
-          return;
-        }
+					initialize();
 
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
+					$scope.changeState = function()
+					{
+						console.log("hallo ");
+						$state.go('question');
+					}
+					
 
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-      };
-      
-      $scope.clickTest = function() {
-        alert('Example of infowindow with ng-click')
-      };
-      
-    });
+					function showspinner() {
+						$ionicLoading.show({
+							content : "whatever"
+						});
+					}
+					;
+					$scope.hide = function() {
+						$ionicLoading.hide();
+					};
 
-myApp.controller('QuestionCtrl', function($scope){
+					//showspinner();
+
+//					$scope.centerOnMe = function() {
+//						if (!$scope.map) {
+//							return;
+//						}
+//
+//						$scope.loading = $ionicLoading.show({
+//							content : 'Getting current location...',
+//							showBackdrop : false
+//						});
+//
+//						navigator.geolocation.getCurrentPosition(
+//								function(pos) {
+//									$scope.map
+//											.setCenter(new google.maps.LatLng(
+//													pos.coords.latitude,
+//													pos.coords.longitude));
+//									$scope.loading.hide();
+//								}, function(error) {
+//									alert('Unable to get location: '
+//											+ error.message);
+//								});
+//					};
+//
+//					$scope.clickTest = function() {
+//						alert('Example of infowindow with ng-click')
+//					};
+
+				});
+
+myApp.controller('QuestionCtrl', function($scope) {
 	console.log("lalal");
 })
 
 myApp.controller('LoginCtrl', function($scope, $ionicModal, $timeout) {
-	  // Form data for the login modal
-	  $scope.loginData = {};
+	// Form data for the login modal
+	$scope.loginData = {};
 
-	  // Create the login modal that we will use later
-	  $ionicModal.fromTemplateUrl('templates/login.html', {
-	    scope: $scope
-	  }).then(function(modal) {
-	    $scope.modal = modal;
-	  });
+	// Create the login modal that we will use later
+	$ionicModal.fromTemplateUrl('templates/login.html', {
+		scope : $scope
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
 
-	  // Triggered in the login modal to close it
-	  $scope.closeLogin = function() {
-	    $scope.modal.hide();
-	  };
+	// Triggered in the login modal to close it
+	$scope.closeLogin = function() {
+		$scope.modal.hide();
+		// window.location.replace("/stapp/_design/app/index.html#/index");
+	};
 
-	  // Open the login modal
-	  $scope.login = function() {
-	    $scope.modal.show();
-	  };
+	// Open the login modal
+	$scope.login = function() {
+		$scope.modal.show();
+	};
 
-	  // Perform the login action when the user submits the login form
-	  $scope.doLogin = function() {
-	    console.log('Doing login', $scope.loginData);
-
-	    // Simulate a login delay. Remove this and replace with your login
-	    // code if using a login system
-	    $timeout(function() {
-	      $scope.closeLogin();
-	    }, 1000);
-	  };
-	})
+	// Perform the login action when the user submits the login form
+	$scope.doLogin = function() {
+		console.log('Doing login', $scope.loginData);
+		// Simulate a login delay. Remove this and replace with your login
+		// code if using a login system
+		$timeout(function() {
+			$scope.closeLogin();
+		}, 1000);
+	};
+})
