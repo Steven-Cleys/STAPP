dd3a9d01aea5e94d38b72110a9cc8af2e91b345d
Steven-Cleys <stevencleys@gmail.com>
2014-11-21 13:42:53 +0100

    barcode

--- app/_attachments/js/controllers.js
+++ app/_attachments/js/controllers.js
@@ -111,3 +111,17 @@ myApp.controller('LoginCtrl', function($scope, $ionicPopup) {
 		myPopup.close();
 	};
 })
+
+myApp.controller("ExampleController", function($scope, $cordovaBarcodeScanner) {
+ 
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+ 
+});
