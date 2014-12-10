c09a24948954bb5b3462f8601372f7eb41744fc9
Ibtissam Hamdaoui <ibtissam.hamdaoui@student.ap.be>
2014-12-10 17:15:46 +0100

    qr map

--- app/_attachments/js/controllers.js
+++ app/_attachments/js/controllers.js
@@ -1,8 +1,11 @@
 var myApp = angular.module('stapp.controllers', [ 'ui.router', 'ngCordova',
-		'ionic' ])
-var myPopup;
-var qrcode;
+                                                  'ionic' ])
+                                                  var myPopup;
+var qrcode = '4lc';
 var jsonarr = []; //array voor data bij te houden
+var markers = [];
+var start;
+var end;
 
 function makeInfoWindowEvent(map, infowindow, marker) {
 	return function() {
@@ -11,196 +14,244 @@ function makeInfoWindowEvent(map, infowindow, marker) {
 }
 
 myApp
-		.controller(
-				'MapCtrl',
-				function($scope, $ionicLoading, $compile, $http, $ionicPopup,
-						$cordovaBarcodeScanner, $state) {
+.controller(
+		'MapCtrl',
+		function($scope, $ionicLoading, $compile, $http, $ionicPopup,
+				$cordovaBarcodeScanner, $state) {
 
-					function loadQuestions() {
-						$http
-								.jsonp(
-										'http://stapp.cloudant.com/ap/_design/views/_view/questions?callback=JSON_CALLBACK')
-								.then(
-										function(resp) {
-											console.log(resp);
-											window.localStorage['questions'] = JSON
-													.stringify(resp.data.rows);
-										}, function(err) {
-											console.error('ERR', err);
-											console.log(err.status);
-										})
+			function loadQuestions() {
+				$http
+				.jsonp(
+				'http://stapp.cloudant.com/ap/_design/views/_view/questions?callback=JSON_CALLBACK')
+				.then(
+
+						function(resp) {
+							console.log(resp);
+							window.localStorage['questions'] = JSON
+							.stringify(resp.data.rows);
+						}, function(err) {
+							console.error('ERR', err);
+							console.log(err.status);
+						})
 						if (window.localStorage['questions'] != null) {
 							console.log("questions loaded");
 						} else {
 							console.log("questions not loaded");
 						}
 
-						window.localStorage['questionOk'] = "0";
-						window.localStorage['questionNok'] = "0";
+				window.localStorage['questionOk'] = "0";
+				window.localStorage['questionNok'] = "0";
 
-					}
-					
-					showspinner();
-					
-					// if (window.localStorage['questions'] == null) {
-					loadQuestions();
-					// }
+			}
 
-					function initialize() {
-						
-						if (window.localStorage['questions'] == null) {
-							setTimeout(function() {
-								initialize();
-							}, 200);
-						} else {
-							// go do that thing
+			showspinner();
 
-							var myLatlng = new google.maps.LatLng(
-									51.21968667200008, 4.4009229560000449); // to
-							// do,
-							// current
-							// coords
+			// if (window.localStorage['questions'] == null) {
+			loadQuestions();
+			// }
 
-							var mapOptions = {
-								center : myLatlng,
-								zoom : 16,
-								mapTypeId : google.maps.MapTypeId.ROADMAP
-							};
 
-							var map = new google.maps.Map(document
-									.getElementById("map"), mapOptions);
+			function initialize() {
+				directionsDisplay = new google.maps.DirectionsRenderer();
+				if (window.localStorage['questions'] == null) {
+					setTimeout(function() {
+						initialize();
+					}, 200);
+				} else {
+					// go do that thing
 
-							jsonarr = JSON
-									.parse(window.localStorage['questions']);
-							for (i = 0; i < jsonarr.length; i++) {
+					var myLatlng = new google.maps.LatLng(
+							51.21968667200008, 4.4009229560000449); // to
+					// do,
+					// current
+					// coords
 
-								var spot = jsonarr[i];
-								var myLatLng = new google.maps.LatLng(
-										spot.value.lat, spot.value.lon);
-
-								var marker = new google.maps.Marker({
-									map : map,
-									position : myLatLng,
-									title : spot.value.hotspot
-								});
-								var infowindow = new google.maps.InfoWindow({
-									content : spot.value.hotspot + "<br>"
-											+ spot.value.adres
-								});
-								console.log(spot.value.adres);
-								marker.setMap(map);
-								google.maps.event.addListener(marker, 'click',
-										makeInfoWindowEvent(map, infowindow,
-												marker));
-
-							}
-							
-							$scope.map = map;
-
-							google.maps.event.addListenerOnce(map,
-									'tilesloaded', function() {
-										$ionicLoading.hide();
-
-										console.log("map loaded");
-
-									});
-						}
-						;
-
-					}
-					
-					initialize();
-					
-
-					if (localStorage.getItem('logins') != null) {
-						console.log(localStorage.getItem('logins'));
-					} else {
-						console.log("its empty");
-						myPopup = $ionicPopup.show({
-							templateUrl : 'templates/login.html',
-							scope : $scope
-						});
-					}
-
-					function showspinner() {
-						$ionicLoading.show({
-							templateUrl : 'templates/loading.html',
-							duration : 3000
-						});
-					}
-					;
-
-					$scope.scanBarcode = function() {
-						var found = false;
-						$cordovaBarcodeScanner
-								.scan()
-								.then(
-										function(imageData) {
-											for (i = 0; i < jsonarr.length; i++) {
-												if (imageData.text == jsonarr[i].value.qrCode) {
-													qrcode = imageData.text;
-													$state.go('question');
-													console
-															.log("Barcode Format -> "
-																	+ imageData.format);
-													console
-															.log("Cancelled -> "
-																	+ imageData.cancelled);
-													found = true;
-													break;
-												}
-
-												if (found == false && i>8) {
-													var alertPopup;
-													alertPopup = $ionicPopup
-															.alert({
-																title : 'Ongeldige QR-Code',
-																buttons : [ {
-																	text : imageData.text,
-																	type : 'button-assertive'
-																	}
-																 ]
-															});
-											}
-											}
-
-										},
-										function(error) {
-											console.log("An error happened -> "
-													+ error);
-										});
+					var mapOptions = {
+							center : myLatlng,
+							zoom : 16,
+							mapTypeId : google.maps.MapTypeId.ROADMAP
 					};
 
-					// showspinner();
+					var map = new google.maps.Map(document
+							.getElementById("map"), mapOptions);
 
-					// $scope.centerOnMe = function() {
-					// if (!$scope.map) {
-					// return;
-					// }
-					//
-					// $scope.loading = $ionicLoading.show({
-					// content : 'Getting current location...',
-					// showBackdrop : false
-					// });
-					//
-					// navigator.geolocation.getCurrentPosition(
-					// function(pos) {
-					// $scope.map
-					// .setCenter(new google.maps.LatLng(
-					// pos.coords.latitude,
-					// pos.coords.longitude));
-					// $scope.loading.hide();
-					// }, function(error) {
-					// alert('Unable to get location: '
-					// + error.message);
-					// });
-					// };
-					//
-					// $scope.clickTest = function() {
-					// alert('Example of infowindow with ng-click')
-					// };
+					
 
+					jsonarr = JSON
+					.parse(window.localStorage['questions']);
+					for (i = 0; i < jsonarr.length; i++) {
+
+						var spot = jsonarr[i];
+						var myLatLng = new google.maps.LatLng(
+								spot.value.lat, spot.value.lon);
+
+						var marker = new google.maps.Marker({
+							map : map,
+							position : myLatLng,
+							title : spot.value.hotspot,
+							id: spot.value.qrCode
+						});
+						console.log(marker.id);
+
+						var infowindow = new google.maps.InfoWindow({
+							content : spot.value.hotspot + "<br>"
+							+ spot.value.adres
+						});
+						console.log(spot.value.adres);
+						marker.setMap(map);
+						google.maps.event.addListener(marker, 'click',
+								makeInfoWindowEvent(map, infowindow,
+										marker));
+
+						markers.push(marker);
+					}
+
+					$scope.map = map;
+
+					google.maps.event.addListenerOnce(map,
+							'tilesloaded', function() {
+						$ionicLoading.hide();
+
+						console.log("map loaded");
+
+					});
+				}
+				;
+
+			}
+
+			initialize();
+
+			var directionsDisplay;
+			var directionsService = new google.maps.DirectionsService();
+
+			function calcRoute(start,end) {
+				
+				var request = {
+						origin:start,
+						destination:end,
+						travelMode: google.maps.TravelMode.WALKING
+				};
+				directionsService.route(request, function(response, status) {
+					if (status == google.maps.DirectionsStatus.OK) {
+						directionsDisplay.setDirections(response);
+					}
 				});
+			}
+
+			if (qrcode != null){
+
+				for (i = 0; i < markers.length; i++) {
+					if(markers[i].id == qrcode){
+						start = markers[i].position;
+						
+						if(i == (markers.length -1)){
+							end = markers[0].position;
+						}
+						else 
+						{
+							end = markers[i+1].position;
+						}
+						
+						console.log(start);
+						console.log(end);
+						
+					}
+				}
+				calcRoute(start,end);
+			}
+			
+
+
+			if (localStorage.getItem('logins') != null) {
+				console.log(localStorage.getItem('logins'));
+			} else {
+				console.log("its empty");
+				myPopup = $ionicPopup.show({
+					templateUrl : 'templates/login.html',
+					scope : $scope
+				});
+			}
+
+			function showspinner() {
+				$ionicLoading.show({
+					templateUrl : 'templates/loading.html',
+					duration : 3000
+				});
+			}
+			;
+
+			$scope.scanBarcode = function() {
+				var found = false;
+				$cordovaBarcodeScanner
+				.scan()
+				.then(
+						function(imageData) {
+							for (i = 0; i < jsonarr.length; i++) {
+								if (imageData.text == jsonarr[i].value.qrCode) {
+									qrcode = imageData.text;
+									$state.go('question');
+									console
+									.log("Barcode Format -> "
+											+ imageData.format);
+									console
+									.log("Cancelled -> "
+											+ imageData.cancelled);
+									found = true;
+									break;
+								}
+
+								if (found == false && i>8) {
+									var alertPopup;
+									alertPopup = $ionicPopup
+									.alert({
+										title : 'Ongeldige QR-Code',
+										buttons : [ {
+											text : imageData.text,
+											type : 'button-assertive'
+										}
+										]
+									});
+								}
+							}
+
+						},
+						function(error) {
+							console.log("An error happened -> "
+									+ error);
+						});
+			};
+
+			// showspinner();
+
+			// $scope.centerOnMe = function() {
+			// if (!$scope.map) {
+			// return;
+			// }
+			//
+			// $scope.loading = $ionicLoading.show({
+			// content : 'Getting current location...',
+			// showBackdrop : false
+			// });
+			//
+			// navigator.geolocation.getCurrentPosition(
+			// function(pos) {
+			// $scope.map
+			// .setCenter(new google.maps.LatLng(
+			// pos.coords.latitude,
+			// pos.coords.longitude));
+			// $scope.loading.hide();
+			// }, function(error) {
+			// alert('Unable to get location: '
+			// + error.message);
+			// });
+			// };
+			//
+			// $scope.clickTest = function() {
+			// alert('Example of infowindow with ng-click')
+			// };
+
+		});
 
 myApp.controller('QuestionCtrl', function($scope, $ionicPopup, $state) {
 
