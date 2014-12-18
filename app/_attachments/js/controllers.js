c06ee31b2bf7a6d8d3c66a20b4f33dc6b6762e41
Steven-Cleys <stevencleys@gmail.com>
2014-12-18 23:37:30 +0100

    KILL ME PLEASE

--- app/_attachments/js/controllers.js
+++ app/_attachments/js/controllers.js
@@ -26,33 +26,33 @@ myApp
 		function($scope, $ionicModal, $ionicLoading, $http, $ionicPopup, $ionicPopover,
 				$cordovaBarcodeScanner, $state) {
 			
-			 $ionicPopover.fromTemplateUrl('templates/info-popup.html', {
-				    scope: $scope,
-				  }).then(function(popover) {
-				    $scope.popover = popover;
-				  });
-			 
-			 $scope.openPopover = function($eventt) {
-				    $scope.popover.show($event);
-				  };
-				  $scope.closePopover = function() {
-				    $scope.popover.hide();
-				  };
-				  //Cleanup the popover when we're done with it!
-				  $scope.$on('$destroy', function() {
-				    $scope.popover.remove();
-				  });
-				  // Execute action on hide popover
-				  $scope.$on('popover.hidden', function() {
-				    // Execute action
-				  });
-				  // Execute action on remove popover
-				  $scope.$on('popover.removed', function() {
-				    // Execute action
-				  });
+//			 $ionicPopover.fromTemplateUrl('templates/info-popup.html', {
+//				    scope: $scope,
+//				  }).then(function(popover) {
+//				    $scope.popover = popover;
+//				  });
+//			 
+//			 $scope.openPopover = function($eventt) {
+//				    $scope.popover.show($event);
+//				  };
+//				  $scope.closePopover = function() {
+//				    $scope.popover.hide();
+//				  };
+//				  //Cleanup the popover when we're done with it!
+//				  $scope.$on('$destroy', function() {
+//				    $scope.popover.remove();
+//				  });
+//				  // Execute action on hide popover
+//				  $scope.$on('popover.hidden', function() {
+//				    // Execute action
+//				  });
+//				  // Execute action on remove popover
+//				  $scope.$on('popover.removed', function() {
+//				    // Execute action
+//				  });
+//				 
 				 
-				 
-				  setTimeout(function(){ $scope.popover.show();}, 500);
+				  //setTimeout(function(){ $scope.popover.show();}, 500);
 			
 		    $ionicModal.fromTemplateUrl('templates/image-modal.html', function($ionicModal) {
 		        $scope.modal = $ionicModal;
@@ -268,7 +268,7 @@ myApp
 			} else {
 				console.log("its empty");
 
-				
+				$state.go('login');
 				// Create the login modal that we will use later
 
 				
@@ -527,15 +527,15 @@ myApp.controller('QuestionCtrl', function($scope, $ionicPopup, $state) {
 
 myApp.controller('LoginCtrl', function($scope, $ionicModal) {
 	
-	
-	$ionicModal.fromTemplateUrl('templates/login.html', {
-		scope : $scope
-	}).then(function(modal) {
-		$scope.loginModal = modal;
-	});
-	
-	setTimeout(function(){ $scope.loginModal.show();}, 500);
-	
+	console.log("controller executed");
+//	$ionicModal.fromTemplateUrl('templates/login.html', {
+//		scope : $scope
+//	}).then(function(modal) {
+//		$scope.loginModal = modal;
+//	});
+//	
+//	setTimeout(function(){ $scope.loginModal.show();}, 500);
+//	
 	
 
 	// Perform the login action when the user submits the login form
@@ -544,7 +544,6 @@ myApp.controller('LoginCtrl', function($scope, $ionicModal) {
 		// Simulate a login delay. Remove this and replace with your login
 		// code if using a login system
 		localStorage.setItem('logins', JSON.stringify($scope.login));
-
-		myPopup.close();
 	};
 })
+
